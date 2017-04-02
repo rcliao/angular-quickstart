0e8c801e3b81b9f5f97b2fb35a014e3cc9e3e9bd
Eric Liao <rcliao01@gmail.com>
2017-04-02 15:23:03 -0700

    feat: step 5 of hero tour (services)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bambasto' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
+  providers: [HeroService],
   template: `
     <h1>Hello {{title}}</h1>
     <h2>My heroes:</h2>
@@ -21,14 +17,23 @@ const HEROES: Hero[] = [
     <hero-detail [hero]="selectedHero"></hero-detail>
   `,
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then(heroes => {
+        this.heroes = heroes;
+      });
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
