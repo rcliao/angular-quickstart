3fa5baec2f6f9cc2d372e5a123cc55b6136d63de
Eric Liao <rcliao01@gmail.com>
2017-04-02 14:19:26 -0700

    feat: finish hero tour step 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{title}}</h1>
+    <h2>{{hero.name}} detail</h2>
+    <div>
+      <label for="name">Name: </label>
+      <input type="text" [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
